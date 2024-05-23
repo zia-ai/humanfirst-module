6f6260458076b957eba6a3048a0571cc97c13c97
faya98 <101306027+faya98@users.noreply.github.com>
2024-05-23 19:48:15 +0530

    Update parent ID property

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "humanfirst", "README.md"), encoding="utf-8") as f:
     long_description = "\n" + f.read()
 
-VERSION = '1.1.0'
+VERSION = '1.1.1'
 DESCRIPTION = 'HumanFirst Package Module'
 
 CLASSIFIERS = [
